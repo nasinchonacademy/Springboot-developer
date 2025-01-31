b67bfe5524d0f22376c9c4983dc423e72aff0e53
nasil <sktlf113@naver.com>
2025-01-31 21:57:19 +0900

    TestControllerTest 작성

---
+++ src/test/java/org/zerock/springbootdeveloper/controller/TestControllerTest.java
@@ -0,0 +1,38 @@
+package org.zerock.springbootdeveloper.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.zerock.springbootdeveloper.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+}
