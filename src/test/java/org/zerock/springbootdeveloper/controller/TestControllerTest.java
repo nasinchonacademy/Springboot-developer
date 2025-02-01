bfac2bc4c68869ebaebf0add92d510864c39babf
nasil <sktlf113@naver.com>
2025-02-01 20:32:42 +0900

    test로직작성 및 컨트롤러추가

--- src/test/java/org/zerock/springbootdeveloper/controller/TestControllerTest.java
+++ src/test/java/org/zerock/springbootdeveloper/controller/TestControllerTest.java
@@ -2,13 +2,22 @@ package org.zerock.springbootdeveloper.controller;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.zerock.springbootdeveloper.dao.Member;
 import org.zerock.springbootdeveloper.repository.MemberRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,5 +43,24 @@ class TestControllerTest {
     @AfterEach
     public void cleanUp() {
         memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result
+                .andExpect(status().isOk())
+                // 응답의 0번째 값이 DB에 저장한 값과 같은지 확인
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
     }
 }
